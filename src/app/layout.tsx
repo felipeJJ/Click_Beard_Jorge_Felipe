fdbc0315711dfd94b6e4a7d6d4965ce4b193ad3a
felipeJJ <jorgefelipe11@hotmail.com>
2024-08-20 14:33:12 -0300

    feat: novas fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,26 @@
+import { Saira, Saira_Stencil_One } from "next/font/google";
 import "./globals.css";
 
+const sairaStencil = Saira_Stencil_One({
+    weight: ["400"],
+    subsets: ["latin"],
+    variable: "--font-saira-stencil",
+});
+
+const saira = Saira({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600"],
+    variable: "--font-saira",
+});
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html className={`${sairaStencil.variable} ${saira.variable}`}>
+            <body>{children}</body>
+        </html>
+    );
 }
