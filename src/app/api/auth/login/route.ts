d83904830acf2d807b1ad313b30e8134e089b964
felipeJJ <jorgefelipe11@hotmail.com>
2024-08-20 19:15:04 -0300

    fix: lidando com a autenticação JWT

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,52 +1,53 @@
+import { NextResponse } from "next/server";
+import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { setCookie } from "nookies";
-import { NextResponse } from "next/server";
 
 const user = {
     id: "1",
-    email: "user@example.com",
-    password: bcrypt.hashSync('password123', 8),
+    email: "1",
+    password: bcrypt.hashSync("1", 8),
 };
 
-export async function POST(
-    req: Request,
-    res: Response
-) {
+export async function POST(req: Request) {
     try {
         const body = await req.json();
         const { email, password } = body;
 
-        if (email !== user.email || !bcrypt.compareSync(password, user.password)) {
+        if (
+            email !== user.email ||
+            !bcrypt.compareSync(password, user.password)
+        ) {
             throw new Error("Email ou senha inválidos");
         }
-    
-        const token = jwt.sign(
-            { id: user.id, email: user.email },
-            process.env.JWT_SECRET || "default-secret",
-            {
-                expiresIn: "1h",
-            }
-        );
-    
-        setCookie({ res }, "token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== "development",
-            maxAge: 3600,
-            path: "/",
-        });
 
-        return(
-            NextResponse.json(
-                { message: "Login bem-sucedido"},
+        if (!process.env.JWT_SECRET) {
+            throw new Error("JWT_SECRET is not defined");
+        } else {
+            const token = jwt.sign(
+                { id: user.id, email: user.email },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "1h",
+                }
+            );
+            const cookie = serialize("token", token, {
+                secure: process.env.NODE_ENV !== "development",
+                maxAge: 3600,
+                path: "/",
+            });
+            const response = NextResponse.json(
+                { message: "Login bem-sucedido" },
                 { status: 200 }
-            )
-        );
+            );
+            response.headers.append("Set-Cookie", cookie);
+    
+            return response;
+        }
     } catch (error: unknown) {
         return NextResponse.json(
             { message: "Erro ao fazer Login", error: (error as Error).message },
             { status: 500 }
-        )
+        );
     }
 }
-
