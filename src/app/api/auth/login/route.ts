99839647d6913d926862e8700ed0d0c2ef374d37
felipeJJ <jorgefelipe11@hotmail.com>
2024-08-20 23:30:35 -0300

    feat: add metodo GET para login, verifica existencia do usuário no banco

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,56 +1,56 @@
 import { NextResponse } from "next/server";
 import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
-
-const users = [
-    {
-        id: "1",
-        email: "1",
-        password: bcrypt.hashSync("1", 8),
-        role: "user",
-    },
-    {
-        id: "2",
-        email: "2",
-        password: bcrypt.hashSync("2", 8),
-        role: "admin",
-    },
-];
+import axios from "axios";
 
 export async function POST(req: Request) {
+    const api = axios.create({
+        baseURL:
+            process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+    });
+
     try {
         const body = await req.json();
         const { email, password } = body;
 
-        const user = users.find((user) => user.email === email);
-        if (!user || !bcrypt.compareSync(password, user.password)) {
-            throw new Error("Email ou senha inválidos");
-        }
+        const apiResponse = await api.get("/api/users", {
+            params: {
+                email,
+                password,
+            },
+        });
 
-        if (!process.env.JWT_SECRET) {
-            throw new Error("JWT_SECRET is not defined");
+        if (apiResponse.status !== 200) {
+            throw new Error(apiResponse.data.error || "Erro de autenticação");
         } else {
-            const token = jwt.sign(
-                { id: user.id, email: user.email, role: user.role },
-                process.env.JWT_SECRET,
-                {
-                    expiresIn: "1h",
-                }
-            );
-            const cookie = serialize("token", token, {
-                secure: process.env.NODE_ENV !== "development",
-                maxAge: 3600,
-                path: "/",
-            });
-            const response = NextResponse.json(
-                { message: "Login bem-sucedido" },
-                { status: 200 }
-            );
-            response.headers.append("Set-Cookie", cookie);
 
-            return response;
+            const user = apiResponse.data.userId;
+    
+            if (!process.env.JWT_SECRET) {
+                throw new Error("JWT_SECRET is not defined");
+            } else {
+                const token = jwt.sign(
+                    { id: user.id, email: user.email, role: user.role },
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: "1h",
+                    }
+                );
+                const cookie = serialize("token", token, {
+                    secure: process.env.NODE_ENV !== "development",
+                    maxAge: 3600,
+                    path: "/",
+                });
+                const response = NextResponse.json(
+                    { message: "Login bem-sucedido" },
+                    { status: 200 }
+                );
+                response.headers.append("Set-Cookie", cookie);
+    
+                return response;
+            }
         }
+
     } catch (error: unknown) {
         return NextResponse.json(
             { message: "Erro ao fazer Login", error: (error as Error).message },
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: NextRequest) {
 
         const result = await query(
             "INSERT INTO users (name, email, password_hash, role) VALUES ($1, $2, $3, $4) RETURNING user_id",
-            [name, email, passwordHash, "client"]
+            [name, email, passwordHash, "user"]
         );
 
         if (result?.rowCount && result.rowCount > 0) {
@@ -39,3 +39,52 @@ export async function POST(req: NextRequest) {
         );
     }
 }
+
+export async function GET(req: NextRequest) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const email = searchParams.get("email");
+        const password = searchParams.get("password");
+
+        if (!email || !password) {
+            return NextResponse.json(
+                { error: "Email e senha são obrigatórios." },
+                { status: 400 }
+            );
+        }
+
+        const result = await query(
+            "SELECT user_id, password_hash FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (result?.rowCount && result.rowCount > 0) {
+            const user = result.rows[0];
+            const isPasswordValid = bcrypt.compareSync(password, user.password_hash);
+
+            if (isPasswordValid) {
+                return NextResponse.json(
+                    { message: "Usuário autenticado com sucesso!", userId: user.user_id },
+                    { status: 200 }
+                );
+            } else {
+                return NextResponse.json(
+                    { error: "Senha incorreta." },
+                    { status: 401 }
+                );
+            }
+        } else {
+            return NextResponse.json(
+                { error: "Usuário não encontrado." },
+                { status: 404 }
+            );
+        }
+    } catch (error) {
+        console.error("Erro ao verificar o usuário:", error);
+        return NextResponse.json(
+            { error: "Erro ao verificar o usuário." },
+            { status: 500 }
+        );
+    }
+}
+
